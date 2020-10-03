f34b2ccc3bb1d2d2426e8713b664bac7532da5b0
RomanDovgii <romandovgiiwork@gmail.com>
2020-10-03 15:36:10 +0300

    Added react-dom-router and other pages

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,39 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
 import WelcomeScreen from "../welcome-screen/welcome-screen";
+import LoginScreen from "../login-screen/login-screen";
+import ArtistQuestionScreen from "../artist-question-screen/artist-question-screen";
+import GenreQuestionSreen from "../genre-question-screen/genre-question-screen";
+import WinScreen from "../win-screen/win-screen";
+import LoseScreen from "../lose-screen/lose-screen";
 
 const App = (props) => {
   const {errorsCount} = props;
 
   return (
-    <WelcomeScreen errorsCount={errorsCount}/>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <WelcomeScreen errorsCount={errorsCount}/>
+        </Route>
+        <Route exact path="/dev-artist">
+          <ArtistQuestionScreen />
+        </Route>
+        <Route exact path="/dev-genre">
+          <GenreQuestionSreen />
+        </Route>
+        <Route exact path="/login">
+          <LoginScreen />
+        </Route>
+        <Route exact path="/result">
+          <WinScreen />
+        </Route>
+        <Route exact path="/lose">
+          <LoseScreen />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
