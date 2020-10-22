ef66693c4198abfab02dd752f314b040b4b6bff5
RomanDovgii <romandovgiiwork@gmail.com>
2020-10-22 17:24:54 +0300

    Action.js is done

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,13 @@
 export const extend = (a, b) => {
   return Object.assign({}, a, b);
 };
+
+export const isMusicianAnswerCorrect = (question, userAnswer) => {
+  return userAnswer.musician === question.song.musician;
+};
+
+export const isGenreAnswerCorrect = (question, userAnswer) => {
+  return userAnswer.ever((it, i) => {
+    return it === (question.answers[i].genre === question.genre);
+  });
+};
