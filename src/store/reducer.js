a27a34c210f0be0f1f12d966cb71f351606ae746
RomanDovgii <romandovgiiwork@gmail.com>
2020-10-22 17:10:59 +0300

    reducer is done

---
+++ src/store/reducer.js
@@ -0,0 +1,43 @@
+import {MAX_MISTAKE_COUNT, ActionType} from "../utils/const";
+import {extend} from "../utils/utils";
+import {questions} from "../mocks/questions";
+
+
+const initalState = {
+  mistakes: 0,
+  step: 0,
+  questions
+};
+
+const reducer = (state = initalState, action) => {
+  switch (action.type) {
+    case ActionType.INCREMENT_STEP:
+      const nextStep = state.step + action.payload;
+
+      return extend(
+          state,
+          {
+            step: nextStep
+          }
+      );
+    case ActionType.INCREMENT_MISTAKES:
+      const mistakes = state.mistakes + action.payload;
+
+      if (mistakes >= MAX_MISTAKE_COUNT) {
+        return extend({}, initalState);
+      }
+
+      return extend(
+          state,
+          {
+            mistakes: state.mistakes + action.payload
+          }
+      );
+    case ActionType.RESET_GAME:
+      return extend({}, initalState);
+  }
+
+  return state;
+};
+
+export {reducer};
